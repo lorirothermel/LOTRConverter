428da0b5a70320b1c54ffd3e8bbf590a59c6d588
Lori Rothermel <lori@rothermel1.com>
2024-12-26 13:56:33 -0800

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Lori Rothermel on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
